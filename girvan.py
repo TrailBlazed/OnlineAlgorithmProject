c2655e4b701fe84469a65f78844ff9fa1b8d1140
srija-srivast <56181823+srija-srivast@users.noreply.github.com>
2019-11-20 23:10:53 -0600

    to get the address graph

--- girvan.py
+++ girvan.py
@@ -1,18 +1,25 @@
+from random import random
+
 import networkx as nx
 import collections
 import pandas as pd
 import matplotlib.pyplot as mtl
+from networkx.algorithms.community import girvan_newman
+
+
 def girvan(G):
     c= sorted(nx.connected_components(G), key = len, reverse=True)
     l=len(c)
+    n= l
+    print(l)
     #print('number of connected componets are:', c)
-    while(l==1):
+    while(n<=l):
         p=edge_to_remove(G)
         G.remove_edge(*p)
         c= sorted(nx.connected_components(G), key = len, reverse=True)
-        l=len(c)
+        n=len(c)
         #print(('number of connected componets are:',c))
-    return c,l
+    return c,n
 def edge_to_remove(G):
     dict1=nx.edge_betweenness_centrality(G)
     list_of_tuples=list(dict1.items())
@@ -34,14 +41,26 @@ df=pd.read_csv("/home/sarada/PycharmProjects/Online_Blockchain/00000000000000388
 df = df.apply(lambda x: x.astype(str))
 G = nx.from_pandas_edgelist(df, source='s', target='t')
 """
-G= nx.read_adjlist(r"/home/sarada/PycharmProjects/OnlineProject/000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214.txt")
+G= nx.read_adjlist(r"D:\Online Algo\project\graphs\ijson\0000000000000a3290f20e75860d505ce0e948a1d1d846bec7e39015d242884b.txt")
 c,l=girvan(G)
 
 
 print('Number of connected componets are:',l)
+"""
 for i in c:
     print(i)
 nx.draw(G,node_size=5)
 mtl.show()
+"""
+#assing cluster colours
+colors = [(random(), random(), random()) for _i in range(l)]
+nodes = list(G.nodes)
+nodeColor = []
+for node in nodes:
+    for x in range(0, len(c)):
+        if c[x].__contains__(node):
+            nodeColor.append(colors[x])
 
-print(len(list(G.nodes)))
+print(nodeColor)
+nx.draw(G,node_color=nodeColor, node_size =5)
+mtl.show()
