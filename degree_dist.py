c8e95116dc15fff958bd3ccbbeb03ee52af96437
srija-srivast <56181823+srija-srivast@users.noreply.github.com>
2019-12-10 19:16:50 -0600

    degree distribution with comments and plotting done

--- degree_dist.py
+++ degree_dist.py
@@ -1,15 +1,28 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-G = nx.read_adjlist(r"/home/sarada/PycharmProjects/OnlineProject/000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214.txt")
+
+# Read the adjacency list for the bitcoin network graph are undirected graph
+G = nx.read_adjlist(r"D:\Online Algo\project\graphs\ppt\0000000000000751db4fb6228b0b53898c70d5972623e6016e90261e45b694a2.txt")
+# G= nx.read_adjlist(r"D:\Online Algo\project\graphs\actor imdb\actor.csv" , delimiter=',', encoding='cp1252')
+
+# Calculate the fraction of nodes having a given degree
 degrees=G.degree()
 degree_values=sorted(set(dict(degrees).values()))
-print(degree_values)
-#degree_values=range(25)
 histogram=[list(dict(degrees).values()).count(i)/float(nx.number_of_nodes(G)) for i in degree_values]
-#plt.bar(degree_values,histogram)
-plt.plot(degree_values, histogram)
+
+# Plot the degree distribution result
+plt.figure()
+ax2 = plt.axes()
+my_ticks = [i for i in range(len(degree_values))]
+ax2.set_xticks(my_ticks)
+ax2.set_xticklabels(degree_values)
+ax2.plot(my_ticks, histogram)
+ax2.set_title('Degree distribution')
 plt.xlabel('Degree')
 plt.ylabel('Fraction of Nodes')
 plt.show()
+
+
+
 
 
