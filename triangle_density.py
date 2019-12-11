fdfa1d6e438bbc31c3e460ffb213287543849145
tadepask <tadepask@myumanitoba.ca>
2019-12-11 10:33:27 -0600

    triangle density

--- triangle_density.py
+++ triangle_density.py
@@ -12,8 +12,9 @@ def wedge_iterator(graph,vertex=None):
         neighbors = graph.neighbors(node)
         for pair in itertools.combinations(neighbors, 2):
             yield (node, pair)
-        for item in pair:
-            l.append(item)
+
+
+
 def count_triangle(graph):
     n = 0
     for wedge in wedge_iterator(graph):
@@ -25,9 +26,10 @@ if __name__ == '__main__':
     g = nx.read_adjlist(r"/home/sarada/PycharmProjects/OnlineProject/000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214.txt")
     print('Graph info',nx.info(g))
     n = 0
-    start_time = time.time()
     n = count_triangle(g)
-    w= wedge_iterator(g)
+    w=[n for n in wedge_iterator(g)]
+    wn=len(w)
+    triangle_density = (3 * n) / wn
     print('Number of triangles:', n/3)
-    print("Wedges count:",w)
-    print('Time used:', time.time() - start_time)
+    print("Wedges count:",wn)
+    print("triangle density:",triangle_density)
